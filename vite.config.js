c3b9711b61a6a02d33f48b1674a477a44449835f
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-05-02 16:48:07 +0200

    Set vite config.

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    // A 'repo-neve' helyére írd be a GitHub repód pontos nevét per jelek közé!
+    base: "/web-projects/",
+});
